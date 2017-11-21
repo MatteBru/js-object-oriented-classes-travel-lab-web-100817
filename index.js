d2891f9503827a7e6a04f0d951134d90bfea4ca9
MatteBru <mbruni616@gmail.com>
2017-11-21 14:09:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+class Driver {
+  constructor(name, startString) {
+    this.name = name;
+    this.startDate = new Date(startString);
+  };
 
+  // startDate(){
+  //   let d =
+  //   return d;
+  // };
+
+  yearsExperienceFromBeginningOf(year){
+    return Math.floor((new Date(year, 0, 0) - this.startDate)/1000/60/60/24/365);
+  };
+
+};
+
+class Route {
+  constructor(bl, el) {
+    this.beginningLocation = bl;
+    this.endingLocation = el;
+  }
+
+  blocksTravelled(){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+    let vb = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical);
+
+    let hb = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal))
+
+    return hb + vb;
+  };
+
+  estimatedTime(ph){
+    let et;
+    ph ? et = this.blocksTravelled() / 2 : et = this.blocksTravelled() / 3;
+    return et;
+  };
+}
